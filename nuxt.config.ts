dbe0c3a20df6413b67cc00d6f1b6879505202c19
meinardus <brya.seran@gmail.com>
2026-05-16 19:35:08 +0700

    first commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,9 +13,7 @@ export default defineNuxtConfig({
   },
   runtimeConfig: {
   public: {
-    apiBaseUrl:
-      process.env.NUXT_PUBLIC_API_BASE_URL ||
-      "http://kawi.localhost:8000",
+    apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || '/api',
   },
 },
 })
