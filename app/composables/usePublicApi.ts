9a5ab58af7f8873731c637560f244d99c85b1fd9
meinardus <brya.seran@gmail.com>
2026-05-17 15:46:35 +0700

    fix filters sustainability

--- app/composables/usePublicApi.ts
+++ app/composables/usePublicApi.ts
@@ -4,11 +4,18 @@ export function usePublicApi() {
   async function request<T>(
     endpoint: string,
     options: any = {}
-  ) {
+  ): Promise<T> {
+
+    const {
+      query,
+      ...rest
+    } = options
+
     return await $fetch<T>(
       `${config.public.apiBaseUrl}${endpoint}`,
       {
-        ...options,
+        ...rest,
+        query
       }
     )
   }
