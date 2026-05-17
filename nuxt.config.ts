9120807145756d2586cc623d86b695b59667006e
meinardus <brya.seran@gmail.com>
2026-05-17 18:40:16 +0700

    fix api request

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,31 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   ssr: true,
+
   devtools: {
     enabled: import.meta.dev,
   },
+
   css: ['~/assets/css/main.css'],
 
   vite: {
     plugins: [tailwindcss()],
   },
-  runtimeConfig: {
-  public: {
-    apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || '/api',
+
+  experimental: {
+    payloadExtraction: false,
   },
-},
+
+  nitro: {
+    compressPublicAssets: true,
+  },
+
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || '/api',
+    },
+  },
 })
